44a900b929016c46b3dfd20bde816cae1b463fbd
Giannis Panou <giannispanou500@gmail.com>
2025-02-09 20:08:07 +0200

    Cart Final

--- js/cart.js
+++ js/cart.js
@@ -25,7 +25,6 @@ function addToCart(itemName, itemPrice) {
 	saveCart();
 	updateCart();
 	calculateItemCount();
-	console.log('Added to cart:', itemName, 'Price:', itemPrice);
 }
 
 function increaseQuantity(itemName) {
@@ -94,40 +93,65 @@ function updateCart() {
 	cart.forEach((item) => {
 		var cartItemDiv = document.createElement('div');
 		cartItemDiv.classList.add('cart-item');
+		cartItemDiv.classList.add('my-3');
 
 		var nameOfItem = document.createElement('div');
+		nameOfItem.classList.add('my-1')
 		nameOfItem.innerText = item.name;
 
+		var totalItemPrice = item.price * item.count;
 		var priceOfItem = document.createElement('div');
+		priceOfItem.classList.add('mb-2')
 		priceOfItem.innerText = new Intl.NumberFormat('el-GR', {
 			style: 'currency',
 			currency: 'EUR',
-		}).format(item.price);
+		}).format(totalItemPrice);
 
 		var itemCountDiv = document.createElement('div');
+		itemCountDiv.classList.add('my-1')
 		itemCountDiv.innerHTML = 'Quantity: ' + item.count;
 
 		var increaseButton = document.createElement('button');
+		increaseButton.classList.add('increaseButton');
+		increaseButton.classList.add('btn');
+		increaseButton.classList.add('btn-light');
+		increaseButton.classList.add('border');
+		increaseButton.classList.add('border-dark');
+		increaseButton.classList.add('rounded-0');
+		increaseButton.classList.add('me-2');
 		increaseButton.innerText = '+';
 		increaseButton.onclick = function () {
 			increaseQuantity(item.name);
 		};
 
 		var decreaseButton = document.createElement('button');
+		decreaseButton.classList.add('decreaseButton');
+		decreaseButton.classList.add('btn');
+		decreaseButton.classList.add('btn-light');
+		decreaseButton.classList.add('border');
+		decreaseButton.classList.add('border-dark');
+		decreaseButton.classList.add('rounded-0');
+		decreaseButton.classList.add('me-2');
 		decreaseButton.innerText = '-';
 		decreaseButton.onclick = function () {
 			decreaseQuantity(item.name);
 		};
 
 		var removeButton = document.createElement('button');
+		removeButton.classList.add('removeButton');
+		removeButton.classList.add('btn');
+		removeButton.classList.add('btn-danger');
+		removeButton.classList.add('border');
+		removeButton.classList.add('border-dark');
+		removeButton.classList.add('rounded-0');
 		removeButton.innerText = 'Remove';
 		removeButton.onclick = function () {
 			removeItem(item.name);
 		};
 
 		cartItemDiv.appendChild(nameOfItem);
-		cartItemDiv.appendChild(priceOfItem);
 		cartItemDiv.appendChild(itemCountDiv);
+		cartItemDiv.appendChild(priceOfItem);
 		cartItemDiv.appendChild(increaseButton);
 		cartItemDiv.appendChild(decreaseButton);
 		cartItemDiv.appendChild(removeButton);
